7814939ada8d8ccd1e610bc00d0c38a9f4ae4b17
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-08-25 08:37:58 +0530

    prototype 1 complete ( its playable now ) with placeholder for the time being

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,21 +2,29 @@ extends CharacterBody2D
 
 var SPEED = 200.0
 var JUMP_VELOCITY = -300.0
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @export var max_jumps = 2
 
+const lines: Array[String] = [
+	"less goo !",
+	"i made it !!",
+	"I am sooo much happy !",
+	"you dont bileve me that i dont know how to speel that 3rd word in this line"
+]
 
 var jumps_done: int = 0
 
 
 func give_powerup():
-	var powerup_duration = 10
+	var powerup_duration = 6
 	
+
+		
 	sprite_2d.scale *= 2
 	sprite_2d.position.y *= 2
 	SPEED = 300
-	JUMP_VELOCITY = -250
+	JUMP_VELOCITY = -450
 	collision.scale *= 2
 	collision.position.y *= 2
 	
@@ -31,30 +39,47 @@ func give_powerup():
 	collision.scale /= 2
 	collision.position.y /= 2
 	
-	
-
-
+	#
+#
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("dialogue"):
+		#DialogManager.start_dialog(global_position, lines)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else: 
 		jumps_done = 0
-
-	# Handle jump.
+	
+	# Jump input
 	if Input.is_action_just_pressed("jump") and jumps_done < max_jumps:		
 		velocity.y = JUMP_VELOCITY
-		jumps_done +=1
-	
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+		jumps_done += 1
+
+	# Movement input
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-		sprite_2d.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# ---- Animation Logic ----
+	if not is_on_floor():
+		if velocity.y < 0:
+			sprite_2d.play("jump")   # going up
+		#else:
+			#sprite_2d.play("fall")   # going down
+	elif direction != 0:
+		sprite_2d.play("run")
+	else:
+		sprite_2d.play("idle")
+
+
+	# Flip sprite
+	if direction < 0:
+		sprite_2d.flip_h = false
+	elif direction > 0:
+		sprite_2d.flip_h = true
 
 	move_and_slide()
--- scripts/main.gd
+++ scripts/main.gd
@@ -14,3 +14,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_kill_zone_below_world_body_entered(body: Node2D) -> void:
+	pass

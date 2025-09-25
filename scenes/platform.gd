14d1c704d84c8f27b7ebc202eaab79e0d78e803f
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-09-25 08:37:41 +0530

    added my new terrains and envoirnment stuff , remaking v3 of tutorial map

---
+++ scenes/platform.gd
@@ -0,0 +1,23 @@
+extends AnimatableBody2D
+
+@export var texture: Texture2D
+@export var collisionShapeScale: float 
+@export var default: bool = false
+
+
+@onready var moving_platform_sprite: Sprite2D = $MovingPlatformSprite
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+
+func _ready() -> void:
+	if texture:
+		moving_platform_sprite.texture = texture
+		visible = true
+	
+	if collisionShapeScale:
+		collision_shape_2d.scale = scale
+	
+	if default:
+		moving_platform_sprite.visible = true
+	else:
+		moving_platform_sprite.visible = false

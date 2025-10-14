cb8a4c8266da296d6639719cef2f6e69bc575e5d
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-10-14 22:11:39 +0530

    added spikes , fixed some bugs related to spikes , updated HUD UI and Shop UI

--- scenes/components/interactable.gd
+++ scenes/components/interactable.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("interact"
+	#if Input.is_action_just_pressed("intera
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
